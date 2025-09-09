78f2d7d3fa2dce84956b010f5c3827bc04ad4a61
Mohammed Aldaragily <mohammed.aldaragily@gmail.com>
2025-09-09 18:25:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Greeting
+print("Welcome To Brand Name Generator App")
+
+# City
+city = input("Your City: \n")
+
+# Pet Name
+pet_name = input("Your Pet Name: \n")
+
+# Combine City Name and Pet Name
+brand_name = city + " " + pet_name
+print("Brand Name: " + brand_name)
